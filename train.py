9a8c96db4b6f972589326fa0c09fb7ebc5794666
srijitcn <srijit.nair@databricks.com>
2025-01-17 12:35:34 -0500

    mlflow integration

--- train.py
+++ train.py
@@ -40,33 +40,35 @@ class Polynomial3(torch.nn.Module):
 
 def train():
 
-    # Create Tensors to hold input and outputs.
-    x = torch.linspace(-math.pi, math.pi, 2000)
-    y = torch.sin(x)
+     with mlflow.start_run(experiment_id=experiment.experiment_id) as mlflow_run:
+         
+        # Create Tensors to hold input and outputs.
+        x = torch.linspace(-math.pi, math.pi, 2000)
+        y = torch.sin(x)
 
-    # Construct our model by instantiating the class defined above
-    model = Polynomial3()
+        # Construct our model by instantiating the class defined above
+        model = Polynomial3()
 
-    # Construct our loss function and an Optimizer. The call to model.parameters()
-    # in the SGD constructor will contain the learnable parameters (defined 
-    # with torch.nn.Parameter) which are members of the model.
-    criterion = torch.nn.MSELoss(reduction='sum')
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-6)
-    for t in range(2000):
-        # Forward pass: Compute predicted y by passing x to the model
-        y_pred = model(x)
+        # Construct our loss function and an Optimizer. The call to model.parameters()
+        # in the SGD constructor will contain the learnable parameters (defined 
+        # with torch.nn.Parameter) which are members of the model.
+        criterion = torch.nn.MSELoss(reduction='sum')
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-6)
+        for t in range(2000):
+            # Forward pass: Compute predicted y by passing x to the model
+            y_pred = model(x)
 
-        # Compute and print loss
-        loss = criterion(y_pred, y)
-        if t % 100 == 99:
-            print(t, loss.item())
+            # Compute and print loss
+            loss = criterion(y_pred, y)
+            if t % 100 == 99:
+                print(t, loss.item())
 
-        # Zero gradients, perform a backward pass, and update the weights.
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # Zero gradients, perform a backward pass, and update the weights.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-    print(f'Result: {model.string()}')
+        print(f'Result: {model.string()}')
 
 
 train()
